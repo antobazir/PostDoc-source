46979dfeb9caba3741e35e2b3550a04020bcec5a
antobazir <85673350+antobazir@users.noreply.github.com>
2023-04-09 22:31:17 +0200

    Add files via upload

--- abm_octave_2D.m
+++ abm_octave_2D.m
@@ -1,4 +1,4 @@
-sz = 10;
+sz = 20;
 Grid = zeros(sz,sz);
 
 cr = 7.5; % cell radius
@@ -7,20 +7,30 @@ c_idx = 1; %cell index
 n_cell = 1;
 %moore neighboorhood
 
+row = ((1:sz).*ones(sz,1))';
+col = (1:sz).*ones(sz,1);
+
 %initialisation
 Grid(sz/2,sz/2)=1;
 
-while(c_idx<6)
-  idx = find(Grid!=0)
+while(c_idx<sz)
+   idx = find(Grid!=0);
+   discrepancy=length(1:c_idx)-length(idx)
+   goal  = 1:c_idx
+   truth = sort(Grid(idx))
   for i = 1:length(idx)
    idx(i);
-   pos(2) = floor(idx(i)/sz)+1;
-   pos(1) = idx(i)-(pos(2)-1)*10;
-   pos
+   pos(2) = col(idx(i));
+   pos(1) = row(idx(i));
+   pos;
     Grid = divide2D(Grid,pos,c_idx+1,sz);
-    Grid
+    Grid;
     c_idx = c_idx+1;
   endfor
   disp("end of loop")
+  imagesc(Grid!=0)
+  waitforbuttonpress
+
 endwhile
+
 
--- divide2D.m
+++ divide2D.m
@@ -1,71 +1,79 @@
 function Grid = divide2D(Grid,pos,c_idx,sz)
   plr = floor(4*rand())+1;
-  plr
+  plr;
     switch(plr)
       case 1
         if(Grid(pos(1)+1,pos(2))!=0)
-          [pos(1)+1 pos(2)]
-          disp('occupied')
-          rim = find(Grid(pos(1)+2:sz,pos(2))==0)
+          [pos(1)+1 pos(2)];
+          %disp('occupied');
+          rim = find(Grid(pos(1)+2:sz,pos(2))==0);
           rim = rim(1);
           rim;
-          pos(1)+rim-1
-          pos(1)+1
+          pos(1)+rim-1;
+          pos(1)+1;
+          before_shift = Grid(:,pos(2))
           for i= pos(1)+2:-1:pos(1)+rim-1
-              disp('done')
+              %disp('done');
               Grid(i+1,pos(2))=Grid(i,pos(2));
           endfor
+          after_shift = Grid(:,pos(2))
          Grid(pos(1)+1,pos(2)) = c_idx;
        else
          Grid(pos(1)+1,pos(2)) = c_idx;
-         '1'
+         %'1'
         endif
       case 2
           if(Grid(pos(1),pos(2)+1)!=0)
-            [pos(1) pos(2)+1]
-            disp('occupied')
-            rim = find(Grid(pos(1),pos(2)+2:sz)==0)
+            [pos(1) pos(2)+1];
+            %disp('occupied');
+            rim = find(Grid(pos(1),pos(2)+2:sz)==0);
             rim = rim(1);
-             pos(2)+rim-1
-            pos(2)+1
+             pos(2)+rim-1;
+            pos(2)+1;
+            before_shift = Grid(pos(1),:)
             for i= pos(2)+2:-1:pos(2)+rim-1
-              disp('done')
+             % disp('done');
               Grid(pos(1),i+1)=Grid(pos(1),i);
             endfor
+            after_shift = Grid(pos(1),:)
           Grid(pos(1),pos(2)+1) = c_idx;
        else
          Grid(pos(1),pos(2)+1) = c_idx;
-         '2'
+         %'2'
         endif
       case 3
          if(Grid(pos(1)-1,pos(2))!=0)
-          [pos(1)-1 pos(2)]
-          disp('occupied')
-          rim = find(Grid(1:pos(1)+1,pos(2))!=0)
+          [pos(1)-1 pos(2)];
+          %disp('occupied');
+          rim = find(Grid(1:pos(1)+1,pos(2))!=0);
           rim = rim(1);
-          rim
+          rim;
+          before_shift = Grid(:,pos(2))
           for i= rim:pos(1)-1
               Grid(i-1,pos(2))=Grid(i,pos(2));
           endfor
+          after_shift = Grid(:,pos(2))
          Grid(pos(1)-1,pos(2)) = c_idx;
        else
          Grid(pos(1)-1,pos(2)) = c_idx;
-         '3'
+         %'3'
         endif
       case 4
           if(Grid(pos(1),pos(2)-1)!=0)
-           [pos(1) pos(2)-1]
-          disp('occupied')
-          rim = find(Grid(pos(1),1:pos(2)-1)!=0)
+           [pos(1) pos(2)-1];
+          %disp('occupied');
+          rim = find(Grid(pos(1),1:pos(2)-1)!=0);
           rim = rim(1);
-          rim
+          rim;
+          before_shift = Grid(pos(1),:)
           for i= rim:pos(2)-1
               Grid(pos(1),i-1)=Grid(pos(1),i);
           endfor
+          after_shift = Grid(pos(1),:)
          Grid(pos(1),pos(2)-1) = c_idx;
        else
         Grid(pos(1),pos(2)-1) = c_idx;
-        '4'
+        %'4'
         endif
     endswitch
     return;
