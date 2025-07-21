53012d8247818b9a68429a070676d111c5c06659
antobazir <85673350+antobazir@users.noreply.github.com>
2025-07-22 01:24:48 +0200

    Add files via upload

---
+++ redac/ocr_mcf7_build.m
@@ -0,0 +1,154 @@
+MCF7_ocr(1).ref =  'Carta2024'
+MCF7_ocr(1).PMID = '39082337'
+MCF7_ocr(1).g_conc = 25e-3
+MCF7_ocr(1).ga_conc = 2e-3
+MCF7_ocr(1).lac_conc0 = 0
+MCF7_ocr(1).Pyr_conc = 0
+MCF7_ocr(1).O_pp = 21;
+MCF7_ocr(1).CO2_pp = 5;
+MCF7_ocr(1).ocr = '7500 pm 500 pmol/Mcell/min'
+MCF7_ocr(1).notes = 'non atp linked'
+MCF7_ocr(1).medium = 'MCF-7 cells were cultured in Dulbecco’s Modified Eagle Medium (DMEM) (#01-057-1A, Sartorius, AG, Göt-tingen, Germany) supplemented with 10% fetal bovine serum (10270-106, Thermo Fisher Scientific , Waltham, MA, USA), 25 mM glucose, 2 mM glutamine, 1 mM pyruvate,'
+MCF7_ocr(1).med = 1% 'DMEM + GlutaMAX'
+MCF7_ocr(1).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(2).ref =  'Carta2024'
+MCF7_ocr(2).PMID = '39082337'
+MCF7_ocr(2).g_conc = 2.5e-3
+MCF7_ocr(2).ga_conc = 0.5e-3
+MCF7_ocr(2).lac_conc0 = 0
+MCF7_ocr(2).Pyr_conc = 0
+MCF7_ocr(2).O_pp = 21;
+MCF7_ocr(2).CO2_pp = 5;
+MCF7_ocr(2).ocr = '7500 pm 1200 pmol/Mcell/min'
+MCF7_ocr(2).notes = 'non atp linked'
+MCF7_ocr(2).medium = 'MCF-7 cells were cultured in Dulbecco’s Modified Eagle Medium (DMEM) (#01-057-1A, Sartorius, AG, Göt-tingen, Germany) supplemented with 10% fetal bovine serum (10270-106, Thermo Fisher Scientific , Waltham, MA, USA), 25 mM glucose, 2 mM glutamine, 1 mM pyruvate,'
+MCF7_ocr(2).med = 1% 'DMEM + GlutaMAX'
+MCF7_ocr(2).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(3).ref =  'Carta2024'
+MCF7_ocr(3).PMID = '39082337'
+MCF7_ocr(3).g_conc = 25e-3
+MCF7_ocr(3).ga_conc = 2e-3
+MCF7_ocr(3).lac_conc0 = 0
+MCF7_ocr(3).Pyr_conc = 0
+MCF7_ocr(3).O_pp = 21;
+MCF7_ocr(3).CO2_pp = 5;
+MCF7_ocr(3).ocr = '10500 pm 500 pmol/Mcell/min'
+MCF7_ocr(3).notes = 'ATP linked'
+MCF7_ocr(3).medium = 'MCF-7 cells were cultured in Dulbecco’s Modified Eagle Medium (DMEM) (#01-057-1A, Sartorius, AG, Göt-tingen, Germany) supplemented with 10% fetal bovine serum (10270-106, Thermo Fisher Scientific , Waltham, MA, USA), 25 mM glucose, 2 mM glutamine, 1 mM pyruvate,'
+MCF7_ocr(3).med = 1% 'DMEM + GlutaMAX'
+MCF7_ocr(3).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(4).ref =  'Carta2024'
+MCF7_ocr(4).PMID = '39082337'
+MCF7_ocr(4).g_conc = 2.5e-3
+MCF7_ocr(4).ga_conc = 0.5e-3
+MCF7_ocr(4).lac_conc0 = 0
+MCF7_ocr(4).Pyr_conc = 0
+MCF7_ocr(4).O_pp = 21;
+MCF7_ocr(4).CO2_pp = 5;
+MCF7_ocr(4).ocr = '11200 pm 1200 pmol/Mcell/min'
+MCF7_ocr(4).notes = 'ATP linked'
+MCF7_ocr(4).medium = 'MCF-7 cells were cultured in Dulbecco’s Modified Eagle Medium (DMEM) (#01-057-1A, Sartorius, AG, Göt-tingen, Germany) supplemented with 10% fetal bovine serum (10270-106, Thermo Fisher Scientific , Waltham, MA, USA), 25 mM glucose, 2 mM glutamine, 1 mM pyruvate,'
+MCF7_ocr(4).med = 1% 'DMEM + GlutaMAX'
+MCF7_ocr(4).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(5).ref =  'Russell2022'
+MCF7_ocr(5).PMID = '35840963'
+MCF7_ocr(5).g_conc = 11e-3
+MCF7_ocr(5).ga_conc = 2.05e-3
+MCF7_ocr(5).lac_conc0 = 0
+MCF7_ocr(5).Pyr_conc = 0
+MCF7_ocr(5).O_pp = 21;
+MCF7_ocr(5).CO2_pp = 5;
+MCF7_ocr(5).g_cons = '11 pm 4 pmol/min/mg protein'
+MCF7_ocr(5).notes = 'basal'
+MCF7_ocr(5).medium = 'RPMI media 1640 (Life Technologies Gibco®, 11,875–093) supplemented with 10% FBS (Hyclone Laboratories, UT) '
+MCF7_ocr(5).med = 2% 'RPMI'
+MCF7_ocr(5).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(6).ref =  'Marini2022'
+MCF7_ocr(6).PMID = '36670904'
+MCF7_ocr(6).g_conc = 25e-3
+MCF7_ocr(6).ga_conc = 2e-3
+MCF7_ocr(6).lac_conc0 = 0
+MCF7_ocr(6).Pyr_conc = 1e-3
+MCF7_ocr(6).O_pp = 21;
+MCF7_ocr(6).CO2_pp = 5;
+MCF7_ocr(6).ocr= "11300 pm 800 pmol/min/Mcell"
+MCF7_ocr(6).ratio_lg = -1
+MCF7_ocr(6).notes = 'basal/seahorse'
+MCF7_ocr(6).medium = 'Dulbecco’s Modified Eagle Medium (DMEM, Gibco, Los Angeles, CA, USA) supplemented with 10% fetal bovine serum, 25 mM glucose, 2 mM glutamine, 1 mM pyruvate, 1% penicillin-streptomycin (Gibco, Grand Island, NY, USA) at 37 °C with 5% CO2.'
+MCF7_ocr(6).med = 1% 'DMEM'
+MCF7_ocr(6).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(7).ref =  'Marini2022'
+MCF7_ocr(7).PMID = '36670904'
+MCF7_ocr(7).g_conc = 25e-3
+MCF7_ocr(7).ga_conc = 2e-3
+MCF7_ocr(7).lac_conc0 = 0
+MCF7_ocr(7).Pyr_conc = 1e-3
+MCF7_ocr(7).O_pp = 21;
+MCF7_ocr(7).CO2_pp = 5;
+MCF7_ocr(7).ocr = "11800 pm 1000 pmol/min/Mcell"
+MCF7_ocr(7).ratio_lg = -1
+MCF7_ocr(7).notes = 'maximal/seahorse'
+MCF7_ocr(7).medium = 'Dulbecco’s Modified Eagle Medium (DMEM, Gibco, Los Angeles, CA, USA) supplemented with 10% fetal bovine serum, 25 mM glucose, 2 mM glutamine, 1 mM pyruvate, 1% penicillin-streptomycin (Gibco, Grand Island, NY, USA) at 37 °C with 5% CO2.'
+MCF7_ocr(7).med = 1% 'DMEM'
+MCF7_ocr(7).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(8).ref =  'DelMarBlanquerRossello2016'
+MCF7_ocr(8).PMID = '26772821'
+MCF7_ocr(8).g_conc = 25e-3
+MCF7_ocr(8).ga_conc = 2e-3
+MCF7_ocr(8).lac_conc0 = 0
+MCF7_ocr(8).Pyr_conc = 0
+MCF7_ocr(8).O_pp = 21;
+MCF7_ocr(8).CO2_pp = 5;
+MCF7_ocr(8).ocr = '4.23 pm 0.4 nmol/MCells/min'
+MCF7_ocr(8).ratio_lg = -1
+MCF7_ocr(8).notes = 'electrode measurements'
+MCF7_ocr(8).medium = 'Dulbecco’s Modified Eagle Medium (DMEM, Gibco, Los Angeles, CA, USA) supplemented with 10% fetal bovine serum, 25 mM glucose, 2 mM glutamine, 1 mM pyruvate, 1% penicillin-streptomycin (Gibco, Grand Island, NY, USA) at 37 °C with 5% CO2.'
+MCF7_ocr(8).med = 1% 'DMEM'
+MCF7_ocr(8).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(9).ref =  'WangH2024'
+MCF7_ocr(9).PMID = '39370816'
+MCF7_ocr(9).g_conc = 5.5e-3
+MCF7_ocr(9).ga_conc = 2e-3
+MCF7_ocr(9).lac_conc0 = 0
+MCF7_ocr(9).Pyr_conc = 0
+MCF7_ocr(9).O_pp = 21;
+MCF7_ocr(9).CO2_pp = 5;
+MCF7_ocr(9).ocr = '11 pm 3 pmol/min/5000cells'
+MCF7_ocr(9).ratio_lg = -1
+MCF7_ocr(9).notes = 'Seahorse over 24hours(?)'
+MCF7_ocr(9).medium = 'Dulbecco’s Modified Eagle Medium (DMEM, Gibco, Los Angeles, CA, USA) supplemented with 10% fetal bovine serum, 25 mM glucose, 2 mM glutamine, 1 mM pyruvate, 1% penicillin-streptomycin (Gibco, Grand Island, NY, USA) at 37 °C with 5% CO2.'
+MCF7_ocr(9).med = 1% 'DMEM'
+MCF7_ocr(9).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
+
+MCF7_ocr(10).ref =  'Prior2014'
+MCF7_ocr(10).PMID = '24586439'
+MCF7_ocr(10).g_conc = 25e-3
+MCF7_ocr(10).ga_conc = 2e-3
+MCF7_ocr(10).lac_conc0 = 0
+MCF7_ocr(10).Pyr_conc = 1e-3
+MCF7_ocr(10).O_pp = 21;
+MCF7_ocr(10).CO2_pp = 5;
+MCF7_ocr(10).ocr = '10.15 pm 0.15 nmol/min/Mcells'
+MCF7_ocr(10).ratio_lg = -1
+MCF7_ocr(10).notes = 'Seahorse over 24hours(?/glutamine conc is not given)'
+MCF7_ocr(10).medium = ' Dulbecco’s modified Eagle medium (DMEM) with glucose (4.5 g/l), sodium pyruvate (110 mg/l) and GlutaMAX (Life Technologies) plus 5% heat-inactivated fetal calf serum (FCS) (Life Technologies).'
+MCF7_ocr(10).med = 1% 'DMEM'
+MCF7_ocr(10).dens0_surf= -1 %cells/cm²
+save('MCF7_ocr_data')
